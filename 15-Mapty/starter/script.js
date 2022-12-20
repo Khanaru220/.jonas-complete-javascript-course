10f9d7342de34b67726e2a75af5f2a12b9a6ca31
tin33392js <calsharp0202@yahoo.com>
2022-12-20 14:20:02 +0700

    implement mockLocation

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const mockPosition = {
+  coords: {
+    accuracy: 1446,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: 52.5082624,
+    longitude: 13.2972544,
+    speed: null,
+  },
+  timestamp: 1567849894270,
+};
+
+/* Use mock object instead
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      console.log(position);
+    },
+    () => {
+      // (TODO) not let app stop: offer user manual input (country name -> location of capital)
+      alert('Could not get your position');
+    }
+  );
+} */
+
+console.log(mockPosition);
