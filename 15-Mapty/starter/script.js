4e18604f42a3cdd50d059bdce7f910a16c55e420
tin33392js <calsharp0202@yahoo.com>
2022-12-28 06:16:22 +0700

    convert getPosition to Promise + handle 2 cases of get location. Dep trai lien

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,58 +11,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-const mockPosition = {
-  coords: {
-    accuracy: 1446,
-    altitude: null,
-    altitudeAccuracy: null,
-    heading: null,
-    latitude: 52.5082624,
-    longitude: 13.2972544,
-    speed: null,
-  },
-  timestamp: 1567849894270,
+const getPosition = () => {
+  return new Promise(resolve =>
+    navigator.geolocation.getCurrentPosition(
+      position => resolve(position),
+      () => {
+        //  (TODO) not let app stop: offer user manual input (country name -> location of capital)
+        //  use OSM API to return data: https://stackoverflow.com/a/63505853/14733188
+        const mockPosition = {
+          coords: {
+            accuracy: 1446,
+            altitude: null,
+            altitudeAccuracy: null,
+            heading: null,
+            latitude: 52.5082624,
+            longitude: 13.2972544,
+            speed: null,
+          },
+          timestamp: 1567849894270,
+        };
+        resolve(mockPosition);
+      }
+    )
+  );
 };
 
-/* Use mock object instead
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-    position => {
-      console.log(position);
-    },
-    () => {
-      /*
-       (TODO) not let app stop: offer user manual input (country name -> location of capital)
-       use OSM API to return data: https://stackoverflow.com/a/63505853/14733188 */ /*
-       
-      alert('Could not get your position');
-    }
-  );
-}
-*/
-const { latitude, longitude } = mockPosition.coords;
-const coords = [latitude, longitude];
-const GGMapURL = `https://www.google.com/maps/@${latitude},${longitude},18.04z`;
-console.log(GGMapURL);
+(async () => {
+  const position = await getPosition();
+  const { latitude, longitude } = position.coords; // (?) is there any way to access property of promise value in one line
+  const coords = [latitude, longitude];
 
-// initilize Leaflet map: enable mouse, touch event (like bone)
-const map = L.map('map').setView(coords, 16);
-
-// add tile layer from OpenStreetMap via URL template (like skin)
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-  maxZoom: 19, // prevent continue scroll-in
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-const marker = L.marker(coords)
-  .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
-
-// (TODO) not zoom when double click on marker
-// add event click to get coordination + move marker
-map.on('click', e => {
-  marker.setLatLng(e.latlng).openPopup();
-  form.classList.remove('hidden');
-});
+  // initilize Leaflet map: enable mouse, touch event (like bone)
+  const map = L.map('map').setView(coords, 16);
+  // add tile layer from OpenStreetMap via URL template (like skin)
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19, // prevent continue scroll-in
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  const marker = L.marker(coords)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .addTo(map);
+  // (TODO) not zoom when double click on marker
+  // add event click to get coordination + move marker
+  map.on('click', e => {
+    marker.setLatLng(e.latlng).addTo(map).openPopup();
+    form.classList.remove('hidden');
+  });
+})();
