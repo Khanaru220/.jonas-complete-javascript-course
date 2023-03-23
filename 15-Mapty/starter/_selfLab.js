ddecad281b3069d95ec7ed5ea5a897a783cffda3
tin33392js <calsharp0202@yahoo.com>
2023-03-23 09:56:26 +0700

    able to switch fields between Cadence and Elevation

--- 15-Mapty/starter/_selfLab.js
+++ 15-Mapty/starter/_selfLab.js
@@ -9,6 +9,23 @@
 // 3. calculate the cadence
 
 const btnSubmit = document.querySelector('.form__btn');
+let currentMeasureInput = inputCadence;
+
+const calculateMeasurement = () => {
+  if (
+    [inputDistance, inputDuration].every(
+      el => el.value !== '' && !isNaN(+el.value) && isFinite(+el.value)
+    )
+  ) {
+    // round to specific decimal: https://stackoverflow.com/a/12830454/14733188
+    currentMeasureInput.value =
+      Math.round((inputDistance.value / inputDuration.value) * 100) / 100;
+    btnSubmit.style.display = 'initial';
+  } else {
+    currentMeasureInput.value = null;
+    btnSubmit.style.display = 'none';
+  }
+};
 
 // init
 // reset form each time load page
@@ -16,20 +33,38 @@ window.addEventListener('load', () => {
   form.reset(); // form of 'script.js'
 });
 
-[inputDistance, inputDuration].forEach(el => {
-  el.addEventListener('input', () => {
-    if (
-      [inputDistance, inputDuration].every(
-        el => el.value !== '' && !isNaN(+el.value) && isFinite(+el.value)
-      )
-    ) {
-      // round to specific decimal: https://stackoverflow.com/a/12830454/14733188
-      inputCadence.value =
-        Math.round((inputDistance.value / inputDuration.value) * 100) / 100;
-      btnSubmit.style.display = 'initial';
-    } else {
-      inputCadence.value = null;
-      btnSubmit.style.display = 'none';
-    }
-  });
+inputType.addEventListener('change', function () {
+  switch (this.value) {
+    case 'running':
+      // Sync result from current measurement
+      inputCadence.value = currentMeasureInput.value;
+
+      // Switch to new measurement
+      currentMeasureInput = inputCadence;
+
+      // Switch display
+      currentMeasureInput
+        .closest('.form__row')
+        .classList.remove('form__row--hidden');
+      inputElevation.closest('.form__row').classList.add('form__row--hidden');
+      break;
+    case 'cycling':
+      inputElevation.value = currentMeasureInput.value;
+
+      currentMeasureInput = inputElevation;
+
+      currentMeasureInput
+        .closest('.form__row')
+        .classList.remove('form__row--hidden');
+      inputCadence.closest('.form__row').classList.add('form__row--hidden');
+      break;
+    default:
+      break;
+  }
+
+  // (TODO) it's seems i could make it less repeat
+});
+
+[(inputDistance, inputDuration)].forEach(el => {
+  el.addEventListener('input', calculateMeasurement);
 });
